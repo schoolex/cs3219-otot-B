7263817694d43893bafaba847d4723cc7976ac9b
Biao Yi <schoolex@hotmail.com>
2022-09-18 01:15:14 +0800

    fix: update gcf serverless func to handle errors

--- busArrivalGcf.py
+++ busArrivalGcf.py
@@ -30,25 +30,41 @@ def getArrivalTimings(busStopCode: str, serviceNo: str) -> list:
     try:
         response = req("GET", f'{BUS_ARRIVAL_URL}?BusStopCode={busStopCode}', headers=headers)
         response = response.json()
-        service = list(filter(lambda x: x['ServiceNo'] == str(serviceNo), response['Services']))
-        if not service:
-            raise Exception("Service not found")
-        currentTime = datetime.now(timezone(timedelta(hours=8)))
-        currentTime = currentTime.replace(tzinfo=None)
-        nextBus = datetime.strptime(service[0]['NextBus']['EstimatedArrival'], '%Y-%m-%dT%H:%M:%S+08:00') - currentTime
-        nextBus2 = datetime.strptime(service[0]['NextBus2']['EstimatedArrival'], '%Y-%m-%dT%H:%M:%S+08:00') - currentTime
-        nextBus3 = datetime.strptime(service[0]['NextBus3']['EstimatedArrival'], '%Y-%m-%dT%H:%M:%S+08:00') - currentTime
-        res = {
-            'nextBus': min(nextBus.seconds//60,60),
-            'nextBus2': min(nextBus2.seconds//60,60),
-            'nextBus3': min(nextBus3.seconds//60,60)
-        }
         # Set CORS headers for the main request
         headers = {
         'Access-Control-Allow-Origin': '*'
         }
+        service = list(filter(lambda x: x['ServiceNo'] == str(serviceNo), response['Services']))
+        if not service:
+            emptyRes = {
+            'nextBus': '-',
+            'nextBus2': '-',
+            'nextBus3': '-'
+            }
+            return (emptyRes, 404, headers)
+        currentTime = datetime.now(timezone(timedelta(hours=8)))
+        currentTime = currentTime.replace(tzinfo=None)
+        nextBus = datetime.strptime(service[0]['NextBus']['EstimatedArrival'], '%Y-%m-%dT%H:%M:%S+08:00') - currentTime
+        if len(service[0]['NextBus2']['EstimatedArrival']) > 1:
+            nextBus2 = datetime.strptime(service[0]['NextBus2']['EstimatedArrival'], '%Y-%m-%dT%H:%M:%S+08:00') - currentTime
+            nextBus2 = min(nextBus2.seconds//60,60)
+        else:
+            nextBus2 = '-'
+        if len(service[0]['NextBus3']['EstimatedArrival']) > 1:
+            nextBus3 = datetime.strptime(service[0]['NextBus3']['EstimatedArrival'], '%Y-%m-%dT%H:%M:%S+08:00') - currentTime
+            nextBus3 = min(nextBus3.seconds//60,60)
+        else:
+            nextBus3 = '-'
+        res = {
+            'nextBus': min(nextBus.seconds//60,60),
+            'nextBus2': nextBus2,
+            'nextBus3': nextBus3
+        }
         return (res,200,headers)
     except Exception as e:
         print(e)
-        return e
+        headers = {
+        'Access-Control-Allow-Origin': '*'
+        }
+        return (e,500,headers)
     
